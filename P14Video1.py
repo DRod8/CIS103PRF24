ea7436df6cbe1c3c128d79203fee01f42d15ccde
DRod8 <drodriguez8@student.ccc.edu>
2024-10-29 18:40:24 -0500

    Create P14Video1.py

---
+++ P14Video1.py
@@ -0,0 +1,23 @@
+class Employee:
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('Dan', 'Rodriguez', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+
+emp_1.fullname()
+print(Employee.fullname(emp_1))
+
+#print(emp_1.email)
+#print(emp_2.email)
+
+#print(emp_1.fullname())
