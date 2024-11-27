c315aa14e899d7e3c36868c06c66ac02db9e0501
DRod8 <drodriguez8@student.ccc.edu>
2024-11-26 20:22:04 -0600

    Update Py19.py

--- Py19.py
+++ Py19.py
@@ -1,22 +1,21 @@
 from tkinter import *
 def main():
+    
     mainwin = Tk()
-    color='pink'
+    color='dark gray'
     wcan = Canvas(mainwin,bg=color, width=600,height=800)
-    wcan.pack()
-    wcan.create_oval(391, 391, 165, 165, fill='white',outline="orange", width=5)
-    
-    wcan.create_oval(250, 250, 196, 196, fill='orange',outline="black", width=5)
-    wcan.create_oval(325, 200, 370, 250, fill='orange',outline="black", width=5)
-    
-    wcan.create_line(281,281, 281, 300, width=10, fill="purple")
-    
-    wcan.create_line(200,350, 350, 350, width=10, fill="gray")
-    wcan.create_line(350,350, 200, 400, width=10, fill="gray")
-    wcan.create_line(200,400, 200, 350, width=10, fill="gray")
 
-   ********CHANGE ALL THE COLORS TO YOUR OWN COLORS****** HE IS OK WITH IT NOT BEING PERFECT SO YOU CAN CHANGE THE NUMBERS!!!!!*****
+    wcan.pack()
+    wcan.create_oval(391, 391, 165, 165, fill='yellow',outline="orange", width=5)
     
- 
+    wcan.create_oval(250, 250, 200, 200, fill='light blue',outline="black", width=5)
+    wcan.create_oval(310, 250, 360, 200, fill='light pink',outline="black", width=5)
+    
+    wcan.create_line(280, 280, 280, 300, width=10, fill="orange")
+    
+    wcan.create_line(200, 325, 350, 325, width=5, fill="red")
+    wcan.create_line(350, 325, 275, 375, width=5, fill="blue")
+    wcan.create_line(200, 325, 275, 375, width=5, fill="green")
+
     mainwin.mainloop()
 main()
